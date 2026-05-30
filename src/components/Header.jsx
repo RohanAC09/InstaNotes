3a3edf07341339c813f8b68e4e1097e51972ba79
Rohan Chinchkar <chinchkarrohan@gmail.com>
2026-05-30 21:03:19 +0530

    Download pdf feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,6 @@ function Header(props) {
   return (
     <header>
       <h1>InstaNotes</h1>
-      <div></div>
       <button onClick={props.onDownloadPDF}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
