83b699ac2b30f3ce11ba52c98990e2e25c9069bc
Rohan Chinchkar <chinchkarrohan@gmail.com>
2026-05-30 20:49:16 +0530

    Download pdf feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import jsPDF from 'jspdf';
 
 function App() {
   const defaultNote = {
@@ -10,7 +11,13 @@ function App() {
     content: ""
   };
   const [note, setNote] = useState(defaultNote);
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([{
+    title: "Fun Fact 1",
+    content: "Your brain consumes about 20% of your body's energy"
+  },{
+    title: "Fun Fact 2",
+    content: "Sea otters hold hands while sleeping so they don't drift apart in the water"
+  }]);
 
   function updateInputField({ name, value }) {
     setNote( (currentNote) => ({ ...currentNote, [name]: value }));
@@ -50,6 +57,23 @@ function App() {
     });
   }
 
+  function handleDownloadPDF() {
+        const pdf = new jsPDF();
+        pdf.text('InstaNotes', 20, 20);
+
+        // Add notes to PDF
+        notes.forEach((noteItem, index) => {
+            const yPos = 30 + index * 20;
+            pdf.text(
+                `* Title: ${noteItem.title}`, 20, yPos);
+            pdf.text(
+                `Content: ${noteItem.content}`, 20, yPos + 10);
+        });
+
+        // Save the PDF
+        pdf.save('InstaNotes.pdf');
+    };
+
   const inputFunctions = {
     onUpdateInputField: updateInputField,
     onAdd: addNote,
@@ -59,7 +83,7 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <Header onDownloadPDF={handleDownloadPDF} />
       <CreateArea note={note} functions={inputFunctions} />
       {notes.map((noteItem, index) => {
         return (
